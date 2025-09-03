b998d08eb0be6647d53dd07f6c0a8e97d8d5a2cb
achraf2 <achraf2@localhost.localdomain>
2025-09-03 05:10:55 +0100

    corrected yamllint workflow

--- scripts/linting.sh
+++ scripts/linting.sh
@@ -4,5 +4,10 @@ mkdir -p yaml-reports
 touch yaml-reports/yamllint-report.log
 FILES=$(git ls-files '*.yml' '*.yaml')
 if [ -n "$FILES" ]; then
-	yamllint $FILES >> yaml-reports/yamllint-report.log
+	yamllint $FILES > yaml-reports/yamllint-report.log
 fi
+one=$(grep error  yaml-reports/yamllint-report.log)
+if (( $one == 1 )); then
+	exit 0
+if [ "${ACCEPT_RISK}" = "true" ]; then
+	exit 0
